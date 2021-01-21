48c4ad04531eaab4670c439f6dac85b08f7d48e5
gaochun <gaochun@dabank.io>
2021-01-21 23:38:40 +0800

    [+]implementing listunspent reply.

--- lws_protocol.h
+++ lws_protocol.h
@@ -10,39 +10,39 @@ extern "C" {
 typedef unsigned char big_num[32];
 typedef big_num ed25519_signature;
 typedef big_num ed25519_public_key;
-typedef big_num ed25519_secret_key;
-typedef big_num curve25519_key;
-typedef big_num key_seed;
-typedef big_num shared_key;
+// typedef big_num ed25519_secret_key;
+// typedef big_num curve25519_key;
+// typedef big_num key_seed;
+// typedef big_num shared_key;
 typedef big_num blake2b_hash;
 
-typedef struct _ServiceReply ServiceReply;
-typedef struct _SyncReply SyncReply;
+// typedef struct _ServiceReply ServiceReply;
+// typedef struct _SyncReply SyncReply;
 
-typedef struct {
-    uint16_t nonce;
-    uint32_t version;
-    uint32_t address_id;
-    unsigned char fork_bitmap[8];
-    key_seed seed;
-} ServiceResult;
+// typedef struct {
+//     uint16_t nonce;
+//     uint32_t version;
+//     uint32_t address_id;
+//     unsigned char fork_bitmap[8];
+//     key_seed seed;
+// } ServiceResult;
 
-typedef struct {
-    uint16_t nonce;
-    unsigned char block_hash[32];
-    uint32_t block_height;
-    uint32_t block_time;
-    uint16_t utxo_num;
-    uint8_t continue_flag;
-} SyncResult;
+// typedef struct {
+//     uint16_t nonce;
+//     unsigned char block_hash[32];
+//     uint32_t block_height;
+//     uint32_t block_time;
+//     uint16_t utxo_num;
+//     uint8_t continue_flag;
+// } SyncResult;
 
-typedef struct {
-    uint16_t nonce;
-    uint8_t error;
-    uint8_t err_code;
-    char txid[65];
-    char *err_desc;
-} SendTxResult;
+// typedef struct {
+//     uint16_t nonce;
+//     uint8_t error;
+//     uint8_t err_code;
+//     char txid[65];
+//     char *err_desc;
+// } SendTxResult;
 
 typedef struct {
     unsigned char uuid[16];
@@ -53,40 +53,41 @@ typedef struct {
     char *data;
 } VchData;
 
-typedef unsigned int (*NonceGet)(const void *ctx);
-typedef unsigned int (*DatetimeGet)(const void *ctx);
-typedef int (*DeviceIDGet)(const void *ctx, char *id);
-typedef int (*ForkGet)(const void *ctx, big_num fork_out);
-typedef int (*PublicKeyGet)(const void *ctx, ed25519_public_key pk_out);
-typedef int (*SharedKeyGet)(const void *ctx, const key_seed seed, const unsigned char *data, const size_t size,
-                            shared_key key);
-typedef int (*Blake2bGet)(const void *ctx, const unsigned char *data, const size_t len, blake2b_hash hash);
-typedef int (*SignEd25519)(const void *ctx, const unsigned char *data, const size_t len, ed25519_signature signature);
+// typedef unsigned int (*NonceGet)(const void *ctx);
+// typedef unsigned int (*DatetimeGet)(const void *ctx);
+// typedef int (*DeviceIDGet)(const void *ctx, char *id);
+// typedef int (*ForkGet)(const void *ctx, big_num fork_out);
+// typedef int (*PublicKeyGet)(const void *ctx, ed25519_public_key pk_out);
+// typedef int (*SharedKeyGet)(const void *ctx, const key_seed seed, const unsigned char *data, const size_t size,
+//                             shared_key key);
+// typedef int (*Blake2bGet)(const void *ctx, const unsigned char *data, const size_t len, blake2b_hash hash);
+// typedef int (*SignEd25519)(const void *ctx, const unsigned char *data, const size_t len, ed25519_signature
+// signature);
 
-// 注册用户回调函数
-// int hook_nonce_get(const NonceGet callback, void *ctx);
-// int hook_datetime_get(const DatetimeGet callback, void *ctx);
-// int hook_device_id_get(const DeviceIDGet callback, void *ctx);
-// int hook_fork_get(const ForkGet callback, void *ctx);
-// int hook_public_key_get(const PublicKeyGet callback, void *ctx);
-// int hook_shared_key_get(const SharedKeyGet callback, void *ctx);
-// int hook_blake2b_get(const Blake2bGet callback, void *ctx);
-// int hook_public_sign_ed25519(const SignEd25519 callback, void *ctx);
+// // 注册用户回调函数
+// // int hook_nonce_get(const NonceGet callback, void *ctx);
+// // int hook_datetime_get(const DatetimeGet callback, void *ctx);
+// // int hook_device_id_get(const DeviceIDGet callback, void *ctx);
+// // int hook_fork_get(const ForkGet callback, void *ctx);
+// // int hook_public_key_get(const PublicKeyGet callback, void *ctx);
+// // int hook_shared_key_get(const SharedKeyGet callback, void *ctx);
+// // int hook_blake2b_get(const Blake2bGet callback, void *ctx);
+// // int hook_public_sign_ed25519(const SignEd25519 callback, void *ctx);
 
-// 初始化sdk 须在用户回调注册完毕后执行
-int lws_protocol_init();
+// // 初始化sdk 须在用户回调注册完毕后执行
+// int lws_protocol_init();
 
-// 服务请求/响应
-size_t lws_service_request(unsigned char *data);
-int lws_service_reply_handle(const unsigned char *data, const size_t len, ServiceResult *result);
+// // 服务请求/响应
+// size_t lws_service_request(unsigned char *data);
+// int lws_service_reply_handle(const unsigned char *data, const size_t len, ServiceResult *result);
 
-// 同步请求/响应
-size_t lws_sync_request(unsigned char *data);
-int lws_sync_reply_handle(const unsigned char *data, const size_t len, SyncResult *result);
+// // 同步请求/响应
+// size_t lws_sync_request(unsigned char *data);
+// int lws_sync_reply_handle(const unsigned char *data, const size_t len, SyncResult *result);
 
-// // 交易请求/响应
-size_t lws_send_tx_request(const char *address_hex, VchData *vch_data, unsigned char *data);
-int lws_send_tx_reply_handle(const unsigned char *data, const size_t len, SendTxResult *result);
+// // // 交易请求/响应
+// size_t lws_send_tx_request(const char *address_hex, VchData *vch_data, unsigned char *data);
+// int lws_send_tx_reply_handle(const unsigned char *data, const size_t len, SendTxResult *result);
 
 // New LWS protocol
 #define VERSION 0x0002
@@ -124,9 +125,11 @@ typedef struct {
     HookSignEd25519 hook_public_sign_ed25519;
 } LWSProtocolHook;
 
+enum Command { ListUnspent = 0x0011, SendTx = 0x0012 };
 typedef struct _LWSProtocol LWSProtocol;
 
 typedef enum {
+    LWSPError_Reply_Too_Short,
     LWSPError_HookSHA256GET_NULL,
     LWSPError_HookForkGet_NULL,
     LWSPError_HookPublicKeyGet_NULL,
@@ -139,11 +142,18 @@ typedef enum {
     LWSPError_Success = 0,
 } LWSPError;
 
+typedef struct {
+    uint16_t version;
+    unsigned char hash[32];
+    uint16_t error;
+    uint16_t command;
+} ReplyInfo;
+
 LWSPError protocol_new(const LWSProtocolHook *hook, LWSProtocol **protocol);
-LWSPError protocol_reply_hash(LWSProtocol *protocol, const unsigned char *data, sha256_hash hash);
 LWSPError protocol_listunspent_request(LWSProtocol *protocol, sha256_hash hash, unsigned char *data, size_t *length);
 LWSPError protocol_sendtx_request(LWSProtocol *protocol, const char *address, const VchData *vch, unsigned char *data,
                                   sha256_hash hash);
+LWSPError protocol_reply_info(LWSProtocol *protocol, const unsigned char *data, const size_t length, ReplyInfo *info);
 LWSPError protocol_listunspent_reply_handle(LWSProtocol *protocol, const unsigned char *data, const size_t len);
 LWSPError protocol_sendtx_reply_handle(LWSProtocol *protocol, const unsigned char *data, const size_t len);
 LWSPError protocol_delete(LWSProtocol *protocol);
--- main.c
+++ main.c
@@ -8,43 +8,132 @@
 #include "crc32.h"
 #include "lws_protocol.h"
 
+static char device_id[12] = {253, 255, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13};
+static char id[sizeof(device_id) * 2 + 1];
+static struct mosquitto *mosq;
 static int run = 1;
+static int connected = 0;
 
-void connect_callback(struct mosquitto *mosq, void *obj, int result)
+static struct RequestBuffer {
+    pthread_mutex_t lock;
+    int flag;  // 0-已处理; 1-未处理
+    int index; //该消息发送次数
+    size_t length;
+    unsigned char buff[1024];
+    unsigned char hash[32];
+} buff;
+
+static int message_sender(struct mosquitto *mosq, const unsigned char *data, const size_t length,
+                          const unsigned char *hash)
+{
+    char hex[32 * 2 + 1];
+    memset(hex, 0x00, 32 * 2 + 1);
+    sodium_bin2hex(hex, 32 * 2 + 1, hash, 32);
+    printf("message_sender hash:%s\n", hex);
+
+    // 发送数据
+    pthread_mutex_lock(&buff.lock);
+    buff.flag = 1;
+    buff.length = length;
+    memcpy(&buff.buff, data, length);
+    memcpy(&buff.hash, hash, 32);
+
+    int ret = mosquitto_publish(mosq, NULL, "lws-test-topic", length, data, 0, 0);
+    if (MOSQ_ERR_SUCCESS != ret) {
+        pthread_mutex_unlock(&buff.lock);
+        return ret;
+    }
+    pthread_mutex_unlock(&buff.lock);
+
+    // 检查是否正确处理
+    int i;
+    for (i = 0; i < 1000; i++) {
+        pthread_mutex_lock(&buff.lock);
+        if (1 == buff.flag) {
+            pthread_mutex_unlock(&buff.lock);
+            usleep(10 * 1000);
+
+            continue;
+        }
+        pthread_mutex_unlock(&buff.lock);
+
+        return 0; // 成功
+    }
+
+    // 超时处理
+    pthread_mutex_lock(&buff.lock);
+    buff.index = 0;
+    buff.flag = 0;
+    buff.length = 0;
+    memset(&buff.buff, 0x00, 1024);
+    memset(&buff.hash, 0x00, 32);
+    pthread_mutex_unlock(&buff.lock);
+
+    return 40; //超时
+}
+
+static int message_receiver(LWSProtocol *protocol, const unsigned char *data, const size_t length)
+{
+    ReplyInfo info;
+    LWSPError error = protocol_reply_info(protocol, data, length, &info);
+    if (LWSPError_Success != error) {
+        return error;
+    }
+    printf("version:%d, error:%d, command:%d\n", info.version, info.error, info.command);
+
+    // char hex[32 * 2 + 1];
+    // memset(hex, 0x00, 32 * 2 + 1);
+    // sodium_bin2hex(hex, 32 * 2 + 1, info.hash, 32);
+    // printf("message_receiver hash:%s\n", hex);
+
+    pthread_mutex_lock(&buff.lock);
+    if (0 != memcmp(buff.hash, info.hash, 32)) {
+        pthread_mutex_unlock(&buff.lock);
+        return 41;
+    }
+
+    buff.index = 0;
+    buff.flag = 0;
+    buff.length = 0;
+    memset(&buff.buff, 0x00, 1024);
+    memset(&buff.hash, 0x00, 32);
+
+    if (ListUnspent == info.command) {
+        error = protocol_listunspent_reply_handle(protocol, data, length);
+    }
+
+    if (SendTx == info.command) {
+    }
+    pthread_mutex_unlock(&buff.lock);
+
+    return 0;
+}
+
+static void connect_callback(struct mosquitto *mosq, void *obj, int result)
 {
     LWSProtocol *protocol = (LWSProtocol *)obj;
     printf("connect callback, rc=%d\n", result);
 
-    sha256_hash hash;
-    unsigned char listunspent_request[1024];
-    size_t length = 0;
-    LWSPError error = protocol_listunspent_request(protocol, hash, listunspent_request, &length);
-    if (LWSPError_Success != error) {
-        return;
-    }
-
-    char hex[length * 2 + 1];
-    memset(hex, 0x00, length * 2 + 1);
-    sodium_bin2hex(hex, length * 2 + 1, listunspent_request, length);
-    printf("listunspent error:%d, length:%ld, hex:%s\n", error, length, hex);
+    connected = 1;
 }
 
-void message_callback(struct mosquitto *mosq, void *obj, const struct mosquitto_message *message)
+static void message_callback(struct mosquitto *mosq, void *obj, const struct mosquitto_message *message)
 {
-    // char *req_topic = (char *)obj;
-    // bool match = 0;
-    // printf("got message '%.*s' for topic '%s'\n", message->payloadlen, (char *)message->payload, message->topic);
+    LWSProtocol *protocol = (LWSProtocol *)obj;
+    // receiver
+    char hex[message->payloadlen * 2 + 1];
+    memset(hex, 0x00, message->payloadlen * 2 + 1);
+    sodium_bin2hex(hex, message->payloadlen * 2 + 1, message->payload, message->payloadlen);
 
-    // mosquitto_topic_matches_sub(req_topic, message->topic, &match);
-    // if (match) {
-    // }
+    printf("got message '%.*s' for topic '%s'\n", message->payloadlen * 2 + 1, hex, message->topic);
+    int rc = message_receiver(protocol, message->payload, message->payloadlen);
+    printf("message_callback return:%d\n", rc);
 }
 
-void publish_callback(struct mosquitto *mosq, void *data, int mid) {}
+static void publish_callback(struct mosquitto *mosq, void *data, int mid) {}
 
 static int hook_did_get(const void *context, unsigned char *id)
 {
-    char device_id[12] = {253, 255, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13};
     memcpy(id, device_id, 12);
 
     return 12;
@@ -86,23 +175,23 @@ static int hook_sign_ed25519(const void *ctx, const unsigned char *data, const s
 
 static void mqtt_thread(LWSProtocol *protocol)
 {
-    struct mosquitto *mosq;
+    // 设置id
+    memset(id, 0x00, sizeof(device_id) * 2 + 1);
+    sodium_bin2hex(id, sizeof(device_id) * 2 + 1, device_id, sizeof(device_id));
+    printf("id hex:%s\n", id);
+
     int rc = 0;
     mosquitto_lib_init();
-    char *topic = "lws-test-topic";
-    mosq = mosquitto_new("123456", true, (void *)protocol);
+    char *topic = id;
+    mosq = mosquitto_new(id, true, (void *)protocol);
 
     if (NULL == mosq) {
         mosquitto_lib_cleanup();
         return;
     }
 
-    // char *user_name = "";
-    // char *password = "";
     char *host = "192.168.199.228";
     int port = 1883;
-
-    // mosquitto_username_pw_set(mosq, user_name, password);
     mosquitto_connect_callback_set(mosq, connect_callback);
     mosquitto_message_callback_set(mosq, message_callback);
     mosquitto_publish_callback_set(mosq, publish_callback);
@@ -131,6 +220,27 @@ static void mqtt_thread(LWSProtocol *protocol)
 
 static void loop(LWSProtocol *protocol)
 {
+    while (1 != connected) {
+        usleep(10 * 1000);
+    }
+
+    sha256_hash hash;
+    unsigned char listunspent_request[1024];
+    size_t length = 0;
+    LWSPError error = protocol_listunspent_request(protocol, hash, listunspent_request, &length);
+    if (LWSPError_Success != error) {
+        return;
+    }
+
+    // char hex[length * 2 + 1];
+    // memset(hex, 0x00, length * 2 + 1);
+    // sodium_bin2hex(hex, length * 2 + 1, listunspent_request, length);
+    // printf("listunspent error:%d, length:%ld, hex:%s\n", error, length, hex);
+
+    // sender
+    int ret = message_sender(mosq, listunspent_request, length, hash);
+    printf("connect message sender return:%d\n", ret);
+
     for (;;) {
         sleep(1);
     }
@@ -155,6 +265,10 @@ int main(int argc, char **argv)
         return EXIT_FAILURE;
     }
 
+    // 初始化发送端buffer
+    memset(&buff, 0x00, sizeof(buff));
+    pthread_mutex_init(&buff.lock, NULL);
+
     // 启动MQTT通讯进程
     pthread_t mqtt_thread_tid;
     int mqtt_thread_rc = pthread_create(&mqtt_thread_tid, NULL, (void *)mqtt_thread, protocol);
@@ -163,7 +277,7 @@ int main(int argc, char **argv)
         return EXIT_FAILURE;
     }
 
-    // 产生并发送数据
+    // 产生&发送数据
     loop(protocol);
 
     if (NULL != protocol) {
